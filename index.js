0a1d89173aa6577815e9b24143f2301f78a1376c
Eric Turner <ericturnerdev@gmail.com>
2016-03-19 13:49:28 +0900

    Add simple div component, basic styling

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-import React from 'react'
+import React from "react"
+import ReactDOM from "react-dom"
+import { createStore } from "redux"
+
+const rootEl = document.getElementById("root")
+
+function render() {
+  ReactDOM.render(
+    <div>
+      Redux is a steely-eyed missile man.
+    </div>,
+    rootEl
+  )
+}
+
+render()
