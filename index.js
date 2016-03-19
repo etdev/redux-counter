84153702c539bb79d5e6a8b8fc5296db079b9200
Eric Turner <ericturnerdev@gmail.com>
2016-03-19 14:32:16 +0900

    Add Counter component, reducer

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { createStore } from "redux"
+import Counter from "./components/Counter"
+import counter from "./reducers"
 
+const store = createStore(counter)
 const rootEl = document.getElementById("root")
 
 function render() {
   ReactDOM.render(
     <div>
-      Redux is a steely-eyed missile man.
+      <p>Redux is a steely-eyed missile man.</p>
+      <Counter
+        value={store.getState()}
+        onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+        onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+      />
     </div>,
     rootEl
   )
 }
 
 render()
+store.subscribe(render)
